dceb2e10d13e77b084b3709e8363c4875d4a218e
Prasanth <prasanthcena968@gmail.com>
2022-09-01 18:32:08 +0530

    second activity added

--- app/src/main/java/com/example/constraintlayout_learn/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout_learn/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.constraintlayout_learn
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val context = this
     override fun onCreate(savedInstanceState: Bundle?) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
             WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickMenu(view: View) {
+        Toast.makeText(this@MainActivity, "Its a toast!", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(context, ProfileViewActivity::class.java)
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/constraintlayout_learn/ProfileViewActivity.kt
@@ -0,0 +1,14 @@
+package com.example.constraintlayout_learn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ProfileViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_view)
+    }
+
+    fun onClickMenu2(view: View) {}
+}
